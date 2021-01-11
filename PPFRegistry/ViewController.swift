6c657a2c394aa67086ecb1013bfcd349ad90bf39
Soichiro.Okada <straycat0914@gmail.com>
2021-01-11 15:55:24 +0900

    Initial Commit

---
+++ PPFRegistry/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PPFRegistry
+//
+//  Created by 岡田宗一郎 on 2021/01/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
